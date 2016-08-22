17895f7279b9e6217496731f371d909bd226731f
Gabe Ochoa <gabeochoa@gmail.com>
2016-08-22 15:05:07 -0400

    Renaming kinesis -> kinesis_tools

---
+++ app/kinesis_tools.rb
@@ -0,0 +1,30 @@
+require 'aws-sdk'
+require 'json'
+
+module KinesisTools
+
+  def self.create_kinesis_stream
+
+  end
+
+  def self.start
+    self.aws_config
+
+    # create kinesis stream
+
+    # create lambda function to post to sumo
+
+    # create s3 bucket (if no specified, need a stop check)
+
+    # create kinesis firehose
+
+    # create lambda function to move logs from kinesis to firehose
+
+    # create
+
+  end
+
+end
+
+# start
+KinesisTools.start
