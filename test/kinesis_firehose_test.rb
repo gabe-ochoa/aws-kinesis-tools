effc2933c9c1f018e29d0e19a4afbdcc0c37993f
Gabe Ochoa <gabeochoa@gmail.com>
2016-08-22 13:50:43 -0400

    [Firehose] Adding compression format and cloud watch logging option

--- test/kinesis_firehose_test.rb
+++ test/kinesis_firehose_test.rb
@@ -24,9 +24,9 @@ class TestKinesisFirehose < Test::Unit::TestCase
     @kinesis_firehose.create(@name, @s3_bucket, @prefix)
   end
 
-  # def test_get_s3_bucket_arn
-  #   @kinesis_firehose.get_s3_bucket_arn(@s3_bucket)
-  # end
+  def test_get_s3_bucket_arn
+    @kinesis_firehose.get_s3_bucket_arn(@s3_bucket)
+  end
 
   # def test_monitoring_setup
   #
