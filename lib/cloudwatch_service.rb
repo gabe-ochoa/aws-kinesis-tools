2b0edff9184cca9b1d581b1cd67509640b32d4de
Gabe Ochoa <gabeochoa@gmail.com>
2016-08-23 15:39:00 -0400

    CloudWatch Service create metric alarm and test

--- lib/cloudwatch_service.rb
+++ lib/cloudwatch_service.rb
@@ -1,10 +1,17 @@
 require_relative '../app/kinesis_tools'
+require_relative './aws_service'
 
 class CloudWatchService
 
-  def initialize
-
+  def initialize(options = {})
+    AwsService.aws_config
+    @alarm_client = Aws::CloudWatch::Client.new(options)
   end
 
+  def create_metric_alarm(options)
+    # Options:
+    # http://docs.aws.amazon.com/AmazonCloudWatch/latest/APIReference/API_PutMetricAlarm.html
+    @alarm_client.put_metric_alarm(options)
+  end
 
 end
--- test/lib/cloudwatch_service_test.rb
+++ test/lib/cloudwatch_service_test.rb
@@ -7,7 +7,35 @@ require 'pry'
 class CloudWatchServiceTest < Test::Unit::TestCase
 
   def setup
+    @service = 'test-kinesis-stream'
+    cloudwatch_options = { stub_responses: true }
+    @cloudwatch_client = CloudWatchService.new(cloudwatch_options)
+    @metric_options = ({
+      alarm_name: 'test-alarm-name', # required
+      alarm_description: 'test-alarm-description',
+      actions_enabled: false,
+      ok_actions: ["ResourceName"],
+      alarm_actions: ["ResourceName"],
+      insufficient_data_actions: ["ResourceName"],
+      metric_name: "MetricName", # required
+      namespace: "Namespace", # required
+      statistic: "SampleCount", # required, accepts SampleCount, Average, Sum, Minimum, Maximum
+      dimensions: [
+        {
+          name: "DimensionName", # required
+          value: "DimensionValue", # required
+        },
+      ],
+      period: 1, # required
+      unit: "Seconds", # accepts Seconds, Microseconds, Milliseconds, Bytes, Kilobytes, Megabytes, Gigabytes, Terabytes, Bits, Kilobits, Megabits, Gigabits, Terabits, Percent, Count, Bytes/Second, Kilobytes/Second, Megabytes/Second, Gigabytes/Second, Terabytes/Second, Bits/Second, Kilobits/Second, Megabits/Second, Gigabits/Second, Terabits/Second, Count/Second, None
+      evaluation_periods: 1, # required
+      threshold: 1.0, # required
+      comparison_operator: "GreaterThanOrEqualToThreshold" # required, accepts GreaterThanOrEqualToThreshold, GreaterThanThreshold, LessThanThreshold, LessThanOrEqualToThreshold
+    })
+  end
 
+  def test_create_metric_alarm
+    @cloudwatch_client.create_metric_alarm(@metric_options)
   end
 
 end
