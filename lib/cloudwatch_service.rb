7b3ea97815f65caac38ed1e940edfefa7c283066
Gabe Ochoa <gabeochoa@gmail.com>
2016-08-23 14:54:38 -0400

    Adding cloudwatchservice files

---
+++ lib/cloudwatch_service.rb
@@ -0,0 +1,10 @@
+require_relative '../app/kinesis_tools'
+
+class CloudWatchService
+
+  def initialize
+
+  end
+
+
+end
---
+++ test/lib/cloudwatch_service_test.rb
@@ -0,0 +1,13 @@
+require_relative "../../lib/cloudwatch_service.rb"
+require_relative "../test_helper"
+require "test/unit"
+require 'mocha/test_unit'
+require 'pry'
+
+class CloudWatchServiceTest < Test::Unit::TestCase
+
+  def setup
+
+  end
+
+end
